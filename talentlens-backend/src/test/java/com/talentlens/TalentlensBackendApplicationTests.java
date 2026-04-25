26f15bb94c883aad378f28b04684613cbba58345
siddarth-ba-72 <siddarth.ba02@gmail.com>
2026-04-25 16:18:39 +0530

    spring boot render ready

--- talentlens-backend/src/test/java/com/talentlens/TalentlensBackendApplicationTests.java
+++ talentlens-backend/src/test/java/com/talentlens/TalentlensBackendApplicationTests.java
@@ -1,18 +1,12 @@
 package com.talentlens;
 
-import com.talentlens.kafka.dto.SourcingRequestMessage;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class TalentlensBackendApplicationTests {
-
-    @MockBean
-    KafkaTemplate<String, SourcingRequestMessage> kafkaTemplate;
 
     @Test
     void contextLoads() {
