65333337fb55f54801e6a2a4434b5c5f19891ec3
siddarth-ba-72 <siddarth.ba02@gmail.com>
2026-04-25 23:22:46 +0530

    debug: add loading fallback and console.log to diagnose blank screen

--- talentlens-client/src/main.tsx
+++ talentlens-client/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+console.log('[TalentLens] main.tsx executing')
+
 const rootEl = document.getElementById('root')!
 
 try {
